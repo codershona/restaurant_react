1e59dadca59c65627eb60a7ba606135924e413e7
codershona <falgunimst95@gmail.com>
2020-09-25 14:38:24 +0200

    react component: Updating the Menu Component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
 
   render() {
     return (
@@ -18,7 +28,7 @@ class App extends Component {
           </div>
         </Navbar>
 
-         <Menu />
+         <Menu dishes={this.state.dishes} />
 
 
       </div>
