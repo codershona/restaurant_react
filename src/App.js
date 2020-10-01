162cdfb3dad21c2c1eeeef4701bfc42f26166f75
codershona <falgunimst95@gmail.com>
2020-10-01 16:26:55 +0200

    styled done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import { Navbar, NavbarBrand } from 'reactstrap';
 // import Menu from './components/MenuComponent';
 import Main from './components/MainComponent';
+import './App.css';
 // import { DISHES } from './shared/dishes';
 // import Dishdetail from './components/DishdetailComponent';
 
