4e9d26720f09a57f833f0c9fe79a051939a7ad93
codershona <falgunimst95@gmail.com>
2020-11-19 18:25:20 +0100

    redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -41,7 +41,11 @@ const DishDetail = (props) => {
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        
+      <RenderComments comments={props.comments}
+        addComment={props.addComment}
+        dishId={props.dish.id}
+      />
                     </div>
                 </div>
             </div>
@@ -55,7 +59,7 @@ const DishDetail = (props) => {
         );
 }
 
-function RenderComments({ comments }) {
+ function RenderComments({comments, addComment, dishId}) {
     if (comments != null) {
         return (
             <div className="container">
@@ -73,7 +77,7 @@ function RenderComments({ comments }) {
                             );
                     })}
                 </ul>
-                <CommentForm />
+                 <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         );
     }
@@ -117,9 +121,9 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
-    }
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    
+}
 
     render() {
         return (
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -17,6 +17,8 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import MoreInfo from './MoreInformationComponent';
 
+import { addComment } from '../redux/ActionCreators';
+
 
 const mapStateToProps = state => {
   return {
@@ -26,6 +28,17 @@ const mapStateToProps = state => {
     leaders: state.leaders
   }
 }
+
+
+
+  const mapDispatchToProps = dispatch => ({
+  
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  
+  });
+
+
+
 
 class Main extends Component {
 
@@ -51,8 +64,10 @@ class Main extends Component {
 
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        addComment={this.props.addComment}
+      />
       );
     };
 
@@ -77,4 +92,4 @@ class Main extends Component {
 }
 
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
