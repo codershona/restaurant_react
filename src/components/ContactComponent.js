8177e38a64f833c785ee7accb23e55755fc8602f
codershona <falgunimst95@gmail.com>
2020-11-19 13:12:38 +0100

    React Redux Form: handle submit values added

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -14,12 +14,11 @@ class Contact extends Component {
     }
 
 
-    handleSubmit(event) {
-        console.log('Current State is: ' + JSON.stringify(this.state));
-        alert('Current State is: ' + JSON.stringify(this.state));
-        event.preventDefault();
+   handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        // event.preventDefault();
     }
-
 
     render() {
 
