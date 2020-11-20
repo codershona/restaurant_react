780e435a5e1de81688277fd20bcce2e258ea6051
codershona <falgunimst95@gmail.com>
2020-11-20 21:02:53 +0100

    React Redux Forms Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -19,6 +19,8 @@ import MoreInfo from './MoreInformationComponent';
 
 import { addComment, fetchDishes } from '../redux/ActionCreators';
 
+import { actions } from 'react-redux-form';
+
 
 const mapStateToProps = state => {
   return {
@@ -35,7 +37,8 @@ const mapStateToProps = state => {
   
     addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
 
-    fetchDishes: () => {dispatch(fetchDishes())}
+    fetchDishes: () => {dispatch(fetchDishes())},
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
   
   });
 
@@ -91,7 +94,7 @@ class Main extends Component {
               <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+               <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
               <Redirect to="/home" />
           </Switch>
         </div>
